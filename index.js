08a725f655a89c4fdcc31e485394f4229c09e2f3
SheeTabz <wmuriithitabitha@gmail.com>
2022-08-19 13:16:55 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const  oppressedWorkers = [
+    "Dopey",
+    "Sneezy",
+    "Happy",
+    "Angry",
+    "Doc",
+    "Lemonjello",
+    "Sleepy"
+  ];
+
+  oppressedWorkers.forEach(function(worker) {
+    console.log(`${worker} wants to form a union !`)
+  })
